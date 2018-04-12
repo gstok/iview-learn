ad0f18b8fd5914f5924eb76c35024714eda6c360
gstok <3081716662@qq.com>
2018-04-12 16:57:27 +0800

    layer完成

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,38 +1,25 @@
 
 <style>
-    /* .box {
-        border: solid 1px black;
-    } */
+        .area {
+            position: fixed;
+            width: 100%;
+            height: 100%;
+        }
+
+    .ivu-layout {
+        width: 100%;
+        height: 100%;
+    }
 </style>
 
 
 <template>
-    <div>
-        <Row :gutter="16">
-            <Col class="box" span="12">col-12</Col>
-            <Col class="box" span="12">col-12</Col>
-        </Row>
-        <Row type="flex">
-            <Col class="box" order="4" span="6">1</Col>
-            <Col class="box" order="3" span="6">2</Col>
-            <Col class="box" order="2" span="6">3</Col>
-            <Col class="box" order="1" span="6">4</Col>
-        </Row>
-        <Row>
-            <Button>Default</Button>
-            <Button loading type="primary">Primary</Button>
-            <Button type="ghost">Ghost</Button>
-            <Button type="dashed">Dashed</Button>
-            <Button type="text">Text</Button>
-            <br><br>
-            <Button type="info">Info</Button>
-            <Button type="success">Success</Button>
-            <Button type="warning">Warning</Button>
-            <Button type="error">Error</Button>
-        </Row>
-        <Row>
-            <Icon type="checkmark" />
-        </Row>
+    <div class="area">
+        <Layout>
+            <Header>Header</Header>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+        </Layout>
     </div>
 </template>
 
